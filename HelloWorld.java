ca56ff902d48df6ce8dc1f287a0eef712efef64c
tausifmansuri <55000306+tausifmansuri@users.noreply.github.com>
2021-08-24 13:20:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 public static void main(String args[])
 {
-	System.out.println("Hello Tausif Welcome Jenkins Traning");
+	System.out.println("*****Hello Tausif Welcome Jenkins Traning*****");
 }
 
 }
